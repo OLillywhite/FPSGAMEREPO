523d26b4cd2c596754d804b5a457e1fdd2a519cc
WienerTickler69 <87618967+WienerTickler69@users.noreply.github.com>
2022-05-09 10:53:48 +0100

    mechanic touch ups

---
+++ Assets/Scripts/Doors/TimeDoorPurchase.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class TimeDoorPurchase : MonoBehaviour
+{
+    public GameObject PerkUI;
+    public GameObject player;
+    public GameObject ElectricityOBJ;
+    public GameObject BrokeBoiUI;
+    public GameObject NoElectricityUI;
+    public float PerkCost = 5000f;
+    bool triggered = false;
+    public AudioSource Purchased;
+    public GameObject door;
+    public Animator dooranimator;
+
+    public void Awake()
+    {
+        dooranimator = door.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        ElectricityEnable Electricityenable = ElectricityOBJ.GetComponent<ElectricityEnable>();
+
+        if (Input.GetKeyDown(KeyCode.E) && triggered && Electricityenable.ElectricityOn)
+        {
+            Purchase();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && triggered && !Electricityenable.ElectricityOn)
+        {
+            NoElectricityUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggered = true;
+            PerkUI.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggered = false;
+            PerkUI.SetActive(false);
+            BrokeBoiUI.SetActive(false);
+            NoElectricityUI.SetActive(false);
+        }
+    }
+
+    void Purchase()
+    {
+        CharacterCash Charactercash = player.GetComponent<CharacterCash>();
+        ElectricityEnable Electricityenable = ElectricityOBJ.GetComponent<ElectricityEnable>();
+
+        if (PerkCost <= Charactercash.Cash && !Electricityenable.ElectricityOn)
+        {
+
+            dooranimator.SetBool("IsPurchased", true);
+            Charactercash.TakeCash(PerkCost);
+            
+            Pickup();
+        }
+
+
+        else
+        {
+            BrokeBoiUI.SetActive(true);
+            PerkUI.SetActive(false);
+        }
+
+    }
+
+    void Pickup()
+    {
+        PerkUI.SetActive(false);
+        Purchased.Play();
+        Destroy(gameObject);
+    }
+}
