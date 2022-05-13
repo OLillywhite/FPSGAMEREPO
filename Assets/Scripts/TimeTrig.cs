c0f37db65c06504a330c6a1c5b4b79f22497ade2
WienerTickler69 <87618967+WienerTickler69@users.noreply.github.com>
2022-05-13 12:48:03 +0100

    time machine stuff

---
+++ Assets/Scripts/TimeTrig.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class TimeTrig : MonoBehaviour
+{
+    public GameObject PerkUI;
+    public GameObject player;
+    bool triggered = false;
+    public AudioSource TimeSound;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && triggered)
+        {
+            Purchase();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggered = true;
+            PerkUI.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggered = false;
+            PerkUI.SetActive(false);
+
+        }
+    }
+
+    void Purchase()
+    {
+        Pickup();      
+    }
+
+    void Pickup()
+    {
+        PerkUI.SetActive(false);
+        TimeSound.Play();
+    }
+}
