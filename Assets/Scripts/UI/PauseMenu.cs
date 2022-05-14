0828eef1ebb837f71b9fcea54a0c1b21724249f5
WienerTickler69 <87618967+WienerTickler69@users.noreply.github.com>
2022-05-14 11:06:45 +0100

    Pause Menu Added

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenuUI;
+    public GameObject UICube;
+    public GameObject HUDUI;
+    public GameObject WaveUI;
+    public static bool GameIsPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        UICube.SetActive(true);
+        HUDUI.SetActive(true);
+        WaveUI.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        UICube.SetActive(false);
+        HUDUI.SetActive(false);
+        WaveUI.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu...");
+        Time.timeScale = 1f;
+        //SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
