9eaad6515639cca920aae80b26349f3b6d0ddc96
WienerTickler69 <87618967+WienerTickler69@users.noreply.github.com>
2022-05-14 14:57:33 +0100

    CursorHide on pause menu fixed

--- Assets/Scripts/CursorHide/CursorHide.cs
+++ Assets/Scripts/CursorHide/CursorHide.cs
@@ -1,13 +1,21 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CursorHide : MonoBehaviour
 {
-    void Start()
+    void Update()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked; // Cursor is now locked to screen
+        if (PauseMenu.GameIsPaused == false)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked; // Cursor is now locked to screen
+        }
+        if (PauseMenu.GameIsPaused == true)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None; // Cursor is now locked to screen
+        }
     }
         
 }
